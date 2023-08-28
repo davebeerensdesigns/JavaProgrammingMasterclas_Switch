9fbeecc79b945a25b67f4e164566392c02d598c1
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2023-08-28 15:24:48 +0200

    switch statement

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int switchValue = 3;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("Was a 3, a 4, or a 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
+            default:
+                System.out.println("Value was not 1, 2, 3, 4, or 5");
+                break;
+        }
+        // more code her
     }
 }
